742a56c9718d65ad1b8cd036cf087dd4c256d443
Fernando <fernando@visualcosita.com>
2020-07-26 08:27:30 +0000

    First experiment

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+git_source(:github) { |name| "https://github.com/#{name}.git" }
+source "https://rubygems.org"
+
+gem 'rpi_gpio', '~> 0.4.0'
+gem 'daemons', '~> 1.3', '>= 1.3.1'
---
+++ misfans.rb
@@ -0,0 +1,68 @@
+require 'daemons'
+require 'rpi_gpio'
+require 'logger'
+
+Daemons.run_proc('misfans.rb') do
+  logger = Logger.new('/var/log/misfans.log')
+
+  RPi::GPIO.set_numbering :board
+
+  pin_num = 11
+  RPi::GPIO.setup pin_num, :as => :output
+
+  module Status
+    ON = 0
+    OFF = 1
+  end
+
+  on_ticks = 0
+  off_ticks = 0
+  temp = 0
+  current_status = Status::OFF
+
+  logger.info { "misfans starting..." }
+
+  begin
+    loop do
+      temp = `vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'`.to_f
+      logger.info { "Temp: #{temp} C" }
+
+      #  Update statei only if necessary past thresholds
+      if current_status == Status::ON && temp < 55  
+        RPi::GPIO.set_low pin_num
+        current_status  = Status::OFF
+        logger.info { "FAN OFF" }
+      elsif current_status == Status::OFF && temp > 72
+        RPi::GPIO.set_high pin_num
+        current_status  = Status::ON
+        logger.info { "FAN ON" }
+      end
+
+      if current_status == Status::ON
+        on_ticks += 1
+      else
+        off_ticks += 1
+      end
+
+      total_ticks = on_ticks + off_ticks
+      if (total_ticks % (6 * 15)) == 0
+        # Report every 10s * 6 * 15 ticks (15 minutes)
+        on_percent = (on_ticks.to_f / total_ticks) * 100
+        off_percent = (off_ticks.to_f / total_ticks) *  100
+        logger.info { "-"*15  }
+        logger.info { "Total ticks: #{total_ticks}" }
+        logger.info { "ON: #{on_ticks} (#{on_percent}%)" }
+        logger.info {  "OFF: #{off_ticks} (#{off_percent}%)" }
+        logger.info { "-"*15  }
+      end
+
+      sleep 10
+    end
+  rescue Exception => e
+    logger.info { "Cleaning up..." }
+  end
+
+  RPi::GPIO.clean_up
+  logger.info { "Bye" }
+end
+
